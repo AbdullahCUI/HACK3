f2107321105438f8f9224f18a1566733513fe59a
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-21 21:42:10 +0500

    declared variable and add switch statment

---
+++ HACK/hack.c
@@ -0,0 +1,35 @@
+//creaat program for monthly data usege
+
+/**
+ * Name:Abdullah
+ * Date:2023/21/10
+ * Section :2D
+ * Department:SE
+ * subject:PF
+  **/
+
+ int main(){
+
+    int days;
+
+    printf("Please Seclect Pakeg=>");
+    printf("Pakegs for 30 days:");
+
+    scanf("%d", & days);
+
+   switch(days){
+
+    printf("Select Option to seclect pakeg");
+
+    case 1:
+        printf("This Pakeg For 30 day,s");
+        printf("200 GB data ");
+        printf("Price is 400:");
+
+        break;
+
+        case 2:
+        printf("")
+    }
+
+ }
